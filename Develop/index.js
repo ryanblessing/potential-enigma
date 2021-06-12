f9ca1141a7cc3a5d40e859cd1579efb60869c4d5
Ryan Blessing <ryanblessing20@yahoo.com>
2021-06-12 10:42:59 -0500

    updated questions, more input!

--- Develop/index.js
+++ Develop/index.js
@@ -4,39 +4,83 @@ const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const userQuestions = () => {
     return inquirer.prompt([{
-        type:'input',
-        name: 'name',
-        message: 'What is the Title of your Project? (required)',
-        validate: projectName => {
-            if(projectName) {
-                return true;
-            } else {
-                console.log('Please enter your Projects Title!');
-                return false;
+            type: 'input',
+            name: 'name',
+            message: 'What is the Title of your Project? (required)',
+            validate: projectName => {
+                if (projectName) {
+                    return true;
+                } else {
+                    console.log('Please enter your Projects Title!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type:'input',
-        name:'description',
-        message:'Please provide a detailed descriptions of your Project (required)',
-        validate: descriptionBlock => {
-            if (descriptionBlock) {
-                return true;
-            } else {
-                console.log('please describe your project for us!')
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a detailed descriptions of your Project (required)',
+            validate: descriptionBlock => {
+                if (descriptionBlock) {
+                    return true;
+                } else {
+                    console.log('please describe your project for us!');
+                }
             }
+        },
+        {
+            type: 'checkbox',
+            name: 'language',
+            message: 'What did you build this project with? (Check all that apply!)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'BOOTSTRAP', 'NODE'],
+            validate: languagesBlock => {
+                if (languagesBlock) {
+                    return true;
+                } else {
+                    console.log("Please tell us what languages this App was built with!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'learning',
+            message: 'Please enter a description of what you learned from this project? (required)',
+            validate: learningBlock => {
+                if (learningBlock) {
+                    return true;
+                } else {
+                    console.log('Some Learning had to be done!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'meaning',
+            message: 'Did this project stand out for you at all or have any special meaning to you?'
+        },
+        {
+            type:'input',
+            name:'link',
+            message:'Please enter your websites GitHub link (Required!)',
+            validate: gitHubLink => {
+                if(gitHubLink) {
+                    return true;
+                } else {
+                    console.log('Please enter the link to your GitHub repository!');
+                    return false;
+                }
+            }
+        },
+        {
+            type:'confirm',
+            name:'endQuestions',
+            message:'Is there anything else you would like to add to your REAM.ME about this project?',
+            default: false
         }
-    },
-    { 
-    type: 'checkbox',
-    name: 'language',
-    message: 'What did you build this project with? (Check all that apply!)',
-    choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'BOOTSTRAP', 'NODE']  
-    }
-])
+    ])
 }
-    
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
