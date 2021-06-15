318073d10ea7992ff6533e0ca12b35f2c64fbea6
Ryan Blessing <ryanblessing20@yahoo.com>
2021-06-15 12:19:07 -0500

    updated the badges function- still not working

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,60 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+ // TODO: Create a function that returns a license badge based on which license is passed in
+ const licenseArray = ['ApacheLicense2', 'MIT', 'ISCLicense', 'GNUGPLv3', 'MozillaPublicLicense2', 'TheUnlicense', 'NoLicense']
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+ function renderLicenseBadge(license) {
+   switch (licenseArray) {
+     case "ApacheLicense2":
+       return (`<img src="https://img.shields.io/badge/License-Apache%202.0-brightgreen"`)
+     case "MIT":
+       return (`<img src="https://img.shields.io/badge/License-MIT-brightgreen"`)
+     case "ISCLicense":
+       return (`<img src="https://img.shields.io/badge/License-ISC%20License%20-brightgreen"`)
+     case "GNUGPLv3":
+       return (`<img src="https://img.shields.io/badge/License-GNU%20GPLv3%20-brightgreen"`)
+     case "MozillaPublicLicense2":
+       return (`<img src="https://img.shields.io/badge/License-Mozilla%20Public%20License%202.0-brightgreen"`)
+     case "The Unlicense":
+       return (`<img src="https://img.shields.io/badge/License-The%20Unlicense-brightgreen"`)
+     case "No License":
+       return ([''])
+   }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+ }
+/*const renderLicenseBadge = licenseChoices => {
+  if(licenseChoices === licenseArray[0]) {
+    return(`<img src="https://img.shields.io/badge/License-Apache%202.0-brightgreen"`)
+  }
+}
+if(licenseChoices === licenseArray[1]) {
+  return(`<img src="https://img.shields.io/badge/License-MIT-brightgreen"`)
+}
+if(licenseChoices === licenseArray[2]) {
+  return(`<img src="https://img.shields.io/badge/License-ISC%20License%20-brightgreen"`)
+}
+if(licenseChoices === licenseArray[3]) {
+  return(`<img src="https://img.shields.io/badge/License-GNU%20GPLv3%20-brightgreen"`)
+}
+if(licenseChoices === licenseArray[4]) {
+  return(`<img src="https://img.shields.io/badge/License-Mozilla%20Public%20License%202.0-brightgreen"`)
+}
+if(licenseChoices === licenseArray[5]) {
+  return(`<img src="https://img.shields.io/badge/License-The%20Unlicense-brightgreen"`)
+}
+if(licenseChoices === licenseArray[6]) {
+  return(``)
+}*/
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `#${data.title}
+ // TODO: Create a function that returns the license link
+ // If there is no license, return an empty string
+ function renderLicenseLink(license) {}
+
+ // TODO: Create a function that returns the license section of README
+ // If there is no license, return an empty string
+ function renderLicenseSection(license) {}
+
+ // TODO: Create a function to generate markdown for README
+ function generateMarkdown(data) {
+   return `#${data.title}
 
   ## Description
   ${data.description}
@@ -36,16 +78,18 @@ function generateMarkdown(data) {
 
   ### Contribution
   ${data.contribution}
+  
 
   ### Testing
   ${data.tests}
 
   ### Licensing 
-  ${data.licenses}
+  ${renderLicenseBadge(data.licenses)}
 
   ### Connect With Me
   For any and all questions please email me @${data.email} or ${data.link} Thank you!
   `;
-}
+ }
 
-module.exports = generateMarkdown;
+
+ module.exports = generateMarkdown;
